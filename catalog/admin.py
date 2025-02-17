cb246928590a9cf8b873bd98a4c39d4fb6f09813
Sergey <nekrasov-nekras-nekrasov@mail.ru>
2025-02-17 21:47:16 +0300

    .

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from catalog.models import Category, Product
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("id","category")
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("id","product")
--- catalog/models.py
+++ catalog/models.py
@@ -1,14 +1,63 @@
 from django.db import models
 
-# class Catalog(models.Model):
-#     first_name = models.CharField(max_length=150, verbose_name="Имя")
-#     last_name = models.CharField(max_length=150, verbose_name_plural="Имя")
-#
-#     def __str__(self):
-#         return f"{self.first_name} {self.last_name}"
-#
-#     class Meta:
-#         verbose_name = ''
-#         verbose_name_plural = ''
-#         ordering = ['last_name']
-#         db_table = 'custom_table_name'
+class Catalog(models.Model):
+    product = models.CharField(
+        max_length=30, verbose_name="Продукт", help_text="Введите название продукта")
+    category = models.ForeignKey('Catalog',
+        on_delete=models.SET_NULL,
+        verbose_name="Категория",
+        help_text="Введите категорию продукта",
+        null=True,
+        blank=True,
+        related_name='catalogs')
+
+
+    class Meta:
+        verbose_name = "Каталог"
+        verbose_name_plural = "Каталоги"
+        ordering = ["category"]
+
+    def __str__(self):
+        return self.product
+
+
+class Product(models.Model):
+    product = models.CharField(
+        max_length=30,
+        verbose_name="Название продукта",
+        help_text="Введите название продукта",
+    )
+    descriptions = models.TextField(
+        verbose_name="Описание продукта",
+        help_text="Введите описание продукта",
+        blank=True,
+        null=True,
+    )
+
+    class Meta:
+        verbose_name = "Продукт"
+        verbose_name_plural = "Продукты"
+
+    def __str__(self):
+        return self.product
+
+
+class Category(models.Model):
+    category = models.CharField(
+        max_length=30,
+        verbose_name="Название категории",
+        help_text="Введите название категории",
+    )
+    descriptions = models.TextField(
+        verbose_name="Описание категории",
+        help_text="Введите описание категории",
+        blank=True,
+        null=True,
+    )
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категория"
+
+    def __str__(self):
+        return self.category
