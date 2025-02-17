dd04b9fa1e65053c2dde8760e5668a17d79c500a
Sergey <nekrasov-nekras-nekrasov@mail.ru>
2025-02-17 19:30:19 +0300

    .

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+# class Catalog(models.Model):
+#     first_name = models.CharField(max_length=150, verbose_name="Имя")
+#     last_name = models.CharField(max_length=150, verbose_name_plural="Имя")
+#
+#     def __str__(self):
+#         return f"{self.first_name} {self.last_name}"
+#
+#     class Meta:
+#         verbose_name = ''
+#         verbose_name_plural = ''
+#         ordering = ['last_name']
+#         db_table = 'custom_table_name'
