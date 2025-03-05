cd5cf89e346ae0b0e8a8181805b882278fa2294a
Sergey <nekrasov-nekras-nekrasov@mail.ru>
2025-03-05 19:28:08 +0300

    .

--- catalog/management/commands/add_products.py
+++ catalog/management/commands/add_products.py
@@ -16,11 +16,18 @@ class Command(BaseCommand):
         call_command('loaddata', 'catalog_fixture.json')
         self.stdout.write(self.style.SUCCESS('Успех успешный'))
 
+        category1, _ = Category.objects.get_or_create(name='Фрукты', description='Сладкие, вкусные', )
+        category2, _ = Category.objects.get_or_create(name='Овощи', description='Спелые, полезные',)
 
-        category3, _ = Category.objects.get_or_create(name='Овощи', description='Спелые, полезные',)
+        product1 = [{'names': 'Яблоки', 'description': 'Сладкие,вкусные', 'category': category1, 'price': 120}, ]
+        product3 = [{'names': 'Огурцы', 'description': 'Зеленые,Крупные', 'category': category2, 'price': 80},]
 
-        product3 = [{'names': 'Огурцы', 'description': 'Зеленые,Крупные', 'category': category3, 'price': 80},
-                    {'names': 'Томаты', 'description': 'Красные,сливовые', 'category': category3, 'price': 180},]
+        for prod_data in product1:
+            prod, created = Product.objects.get_or_create(**prod_data)
+            if created:
+                self.stdout.write(self.style.SUCCESS(f'К успеху пришел ты: {prod.names}'))
+            else:
+                self.stdout.write(self.style.WARNING(f'Есть уже тут: {prod.names}'))
 
         for prod_data in product3:
             prod, created = Product.objects.get_or_create(**prod_data)
