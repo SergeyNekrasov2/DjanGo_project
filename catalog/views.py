ba55bf4f8ebead5eeb77c5920d9e6aca1a8ed8a7
Sergey <nekrasov-nekras-nekrasov@mail.ru>
2025-05-31 18:41:10 +0300

    .

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
-from django.http import HttpResponseRedirect, HttpResponseNotFound
+# from lib2to3.fixes.fix_input import context
+# from gc import get_object
+
+from django.shortcuts import render, get_object_or_404
+# from django.http import HttpResponseRedirect, HttpResponseNotFound
 
 from catalog.models import Product
 
@@ -12,15 +15,16 @@ def contacts(request):
     return render(request, "contacts.html")
 
 
-def index(request):
-    return render(request, "base.html")
+# def index(request):
+#     return render(request, "base.html")
 
-# def create(request):
-#     if request.method == "POST":
-#         product = Product()
-#         product.name = request.POST.get("product")
-#         product.descriptions = request.POST.get("descriptions")
-#         product.category = request.POST.get("category")
-#         product.price = request.POST.get("price")
-#         product.save()
-#     return HttpResponseRedirect("/")
+def products_list(request):
+    products = Product.objects.all()
+    context = {"products": products}
+    return render(request, 'products_list.html', context)
+
+
+def product_detail(request, pk):
+    product = get_object_or_404(Product,pk=pk)
+    context = {'product': product}
+    return render(request, 'products_list.html', context)
