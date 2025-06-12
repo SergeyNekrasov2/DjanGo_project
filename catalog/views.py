c51b6131a26470b5e636ef6113a45faade4ddff5
Sergey <nekrasov-nekras-nekrasov@mail.ru>
2025-06-12 14:33:25 +0300

    .

--- catalog/views.py
+++ catalog/views.py
@@ -26,6 +26,10 @@ class ProductCreateView(CreateView):
     form_class = ProductForm
     success_url = reverse_lazy('catalog:product_list')
 
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super().form_valid(form)
+
 class ProductUpdateView(UpdateView):
     model = Product
     form_class = ProductForm
